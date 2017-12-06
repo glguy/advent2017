106ab04a299d2c5358af02f24bb363aed966a6c7
Eric Mertens <emertens@galois.com>
2017-12-06 10:26:49 -0800

    Use a coordinate and vector type for day3

--- execs/Day03.hs
+++ execs/Day03.hs
@@ -5,8 +5,11 @@ import Data.List (mapAccumL)
 import Data.Maybe (mapMaybe)
 import qualified Data.Map as Map
 
-type Coord = (Int,Int)
-type Vector = (Int,Int)
+-- | Coordinate with x and y component
+data Coord  = C !Int !Int deriving (Eq, Ord, Read, Show)
+
+-- | Vector with x and y component
+data Vector = V !Int !Int deriving (Eq, Ord, Read, Show)
 
 main :: IO ()
 main =
@@ -14,43 +17,46 @@ main =
      print (part1 n)
      print (part2 n)
 
-
-directions :: [Vector]
-directions = [ right , up , left, down ]
-
+-- | Unit vectors
 right, left, down, up :: Vector
-right = ( 1, 0)
-left  = (-1, 0)
-down  = ( 0,-1)
-up    = ( 0, 1)
+right = V 1 0
+left  = V (-1) 0
+up    = V 0 1
+down  = V 0 (-1)
 
+-- | Origin coordinate: 0,0
 origin :: Coord
-origin = (0,0)
+origin = C 0 0
 
 -- | Compute L1 norm of a coordinate - manhattan distance
 --
--- >>> manhattanDist (0,0)
+-- >>> manhattanDist (C 0 0)
 -- 0
--- >>> manhattanDist (1,2)
+-- >>> manhattanDist (C 1 2)
 -- 3
--- >>> manhattanDist (-1,3)
+-- >>> manhattanDist (C (-1) 3)
 -- 4
 manhattanDist :: Coord -> Int
-manhattanDist (x,y) = abs x + abs y
+manhattanDist (C x y) = abs x + abs y
 
 
 -- | Add a vector to a coordinate
 --
--- >>> move (10,20) (3,4)
--- (13,24)
-move :: Vector -> Coord -> Coord
-move (dx,dy) (x,y) = (x+dx, y+dy)
+-- >>> move (C 10 20) (V 3 4)
+-- C 13 24
+move :: Coord -> Vector -> Coord
+move (C x y) (V dx dy) = C (x+dx) (y+dy)
 
 
--- | Coordinates in the order visited starting with the origin
-coords :: [(Int,Int)]
+-- | Coordinates in the spiral order starting with the origin
+--
+-- >>> [C 0 0,C 1 0,C 1 1,C 0 1,C (-1) 1] `Data.List.isPrefixOf` coords
+-- True
+coords :: [Coord]
 coords = scanl move origin movements
   where
+    directions = [right, up, left, down]
+
     movements =
       do (d,n) <- cycle directions `zip` (replicate 2 =<< [1..])
          replicate n d
@@ -59,11 +65,10 @@ coords = scanl move origin movements
 -- | Returns the list of coordinates in the local 3x3 square around a
 -- given coordinate.
 --
--- >>> neighborhood (1,2)
--- [(0,1),(0,2),(0,3),(1,1),(1,2),(1,3),(2,1),(2,2),(2,3)]
+-- >>> neighborhood (C 1 2)
+-- [C 0 1,C 0 2,C 0 3,C 1 1,C 1 2,C 1 3,C 2 1,C 2 2,C 2 3]
 neighborhood :: Coord -> [Coord]
-neighborhood (x,y) = [ (x+dx, y+dy) | dx <- [-1 .. 1]
-                                    , dy <- [-1 .. 1] ]
+neighborhood c = [ move c (V dx dy) | dx <- [-1 .. 1], dy <- [-1 .. 1] ]
 
 
 -- | Find manhattan distance of nth visited coordinate using 1-based counting
