c189a67506cd23f63c906182e994c5afff514bd6
Eric Mertens <emertens@gmail.com>
2017-12-05 21:46:35 -0800

    Add comments for Day06

--- execs/Day06.hs
+++ execs/Day06.hs
@@ -10,13 +10,28 @@ main =
   do input <- parseInput <$> getInput 6
      print (solve input)
 
+-- | Parse input as a tab-separated list of integers
+--
+-- >>> parseInput "1\t2\t3\t4"
+-- [1,2,3,4]
 parseInput :: String -> Vector Int
 parseInput = V.fromList . map read . words
 
+-- | Compute both parts of Day 6
+--
+-- >>> solve (V.fromList [0,2,7,0])
+-- (5,4)
 solve :: Vector Int -> (Int,Int)
 solve = findLoop . iterate step
 
 -- | Computes the steps until a state repeats and also the length of the loop
+--
+-- >>> findLoop [1,2,3,4,5,6,7,5]
+-- (7,3)
+-- >>> findLoop [1,1]
+-- (1,1)
+-- >>> findLoop [0,1,1]
+-- (2,1)
 findLoop :: Ord a => [a] -> (Int,Int)
 findLoop = go Map.empty
   where
@@ -28,6 +43,13 @@ findLoop = go Map.empty
 
 -- | Given a vector representing the memory banks compute the new memory bank
 -- layout.
+--
+-- >>> step (V.fromList [0,2,7,0])
+-- [2,4,1,2]
+-- >>> step (V.fromList [2,4,1,2])
+-- [3,1,2,3]
+-- >>> step (V.fromList [3,1,2,3])
+-- [0,2,3,4]
 step :: Vector Int -> Vector Int
 step xs = V.accum (+) (xs V.// [(i,0)]) [ (j`mod`n, 1) | j <- [i+1 .. i+mx] ]
   where
