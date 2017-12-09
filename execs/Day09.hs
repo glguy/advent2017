9a2f269710f5e05a397d7022d76dd615b8f6ce7d
Eric Mertens <emertens@gmail.com>
2017-12-08 21:50:41 -0800

    Add doctests for 9

--- execs/Day09.hs
+++ execs/Day09.hs
@@ -10,8 +10,29 @@ import Data.Foldable
 main :: IO ()
 main = traverse_ print =<< getParsedInput 9 (parseGroup 1)
 
-type Scores = V2 Int -- group score, garbage count
+-- | Vector of: group score, garbage count
+type Scores = V2 Int
 
+-- | Parse the group string format as defined in Day 9. Parse
+-- result is a vector containing the group score and garbage
+-- character count.
+--
+-- >>> parseMaybe (parseGroup 1) "{}"
+-- Just (V2 1 0)
+-- >>> parseMaybe (parseGroup 1) "{{{}}}"
+-- Just (V2 6 0)
+-- >>> parseMaybe (parseGroup 1) "{{},{}}"
+-- Just (V2 5 0)
+-- >>> parseMaybe (parseGroup 1) "{{{},{},{{}}}}"
+-- Just (V2 16 0)
+-- >>> parseMaybe (parseGroup 1) "{<a>,<a>,<a>,<a>}"
+-- Just (V2 1 4)
+-- >>> parseMaybe (parseGroup 1) "{{<ab>},{<ab>},{<ab>},{<ab>}}"
+-- Just (V2 9 8)
+-- >>> parseMaybe (parseGroup 1) "{{<!!>},{<!!>},{<!!>},{<!!>}}"
+-- Just (V2 9 0)
+-- >>> parseMaybe (parseGroup 1) "{{<a!>},{<a!>},{<a!>},{<ab>}}"
+-- Just (V2 3 17)
 parseGroup :: Int -> Parser Scores
 parseGroup n
   = between (char '{') (char '}')
