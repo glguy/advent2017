617df63654169117db1c8e8b657007ea1435b91f
Eric Mertens <emertens@galois.com>
2017-12-18 09:32:32 -0800

    Update day18

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -1,69 +1,95 @@
-{-# Language RankNTypes #-} -- Lens' type synonym use
+{-# Language RankNTypes      #-} -- Lens' type synonym use in 'reg'
+{-# Language RecordWildCards #-} -- abbreviations in 'part2'
 {-|
 Module      : Main
 Description : Day 18 solution
 Copyright   : (c) Eric Mertens, 2017
 License     : ISC
 Maintainer  : emertens@gmail.com
+
+Day 18 defines a simple programming language with arithmetic operations
+and asynchronous communication.
+
 -}
 module Main where
 
-import Advent
-import Control.Lens (at, non, set, over, Lens')
-import Data.Map (Map)
+import Advent        (getInput)
+import Control.Lens  (view, at, non, set, over, Lens')
+import Data.Map      (Map)
+import Data.Sequence (Seq((:<|)), (|>))
+import Text.Read     (readMaybe)
 import qualified Data.Map as Map
 import qualified Data.Vector as V
-import Text.Read (readMaybe)
-import Data.Sequence (Seq((:<|)), (|>))
 
+-- | Print the solution to both parts of the puzzle. Input file can be
+-- overridden via command-line argument.
 main :: IO ()
 main =
   do input <- V.fromList . map words . lines <$> getInput 18
-     print $ firstSend (program input Map.empty)
-     print $ run 0 mempty (program input (Map.singleton "p" 0))
-                   mempty (program input (Map.singleton "p" 1))
+     let pgm n = runProgram input (Map.singleton "p" n)
+     print (part1 (pgm 0))
+     print (part2 (Sim (pgm 0) (pgm 1) mempty mempty 0))
 
--- | Count the number of sends by program #1
-run :: Integer -> Seq Integer -> Command -> Seq Integer -> Command -> Integer
-run cnt (x:<|xs) p1 ys (Receive _ p2) = run cnt     xs      p1     ys        (p2 x)
-run cnt xs       p1 ys (Send    y p2) = run (cnt+1) xs      p1     (ys |> y) p2
-run cnt xs (Receive _ p1) (y:<|ys) p2 = run cnt     xs      (p1 y) ys        p2
-run cnt xs (Send    x p1) ys       p2 = run cnt     (xs|>x) p1     ys        p2
-run cnt _ _ _ _                       = cnt
-
-(!) :: Map String Integer -> String -> Integer
-(!) m k =
-  case readMaybe k of
-    Just v  -> v
-    Nothing -> Map.findWithDefault 0 k m
-
-data Command = Send Integer Command
-             | Receive Integer (Integer -> Command)
-             | Done
-
-firstSend :: Command -> Integer
-firstSend = go 0
+-- | Compute the last send command that preceeds a non-zero receive command.
+part1 :: Command -> Maybe Integer
+part1 = go Nothing
   where
-    go _ (Send x p) = go x p
-    go s (Receive 0 p) = go s (p 0)
-    go s (Receive _ p) = s
+    go _ (Send x p) = go (Just x) p
+    go s (Recv 0 p) = go s (p 0)
+    go s (Recv _ p) = s
+    go _ Done       = Nothing
 
+data Sim = Sim { p0, p1 :: Command, q1, q2 :: Seq Integer, ctr :: !Int }
+
+-- | Count the number of sends by program #1 when executing program #0 and
+-- program #1 in parallel.
+--
+-- This implementation makes use of the @RecordWildCards@ extension.
+part2 :: Sim -> Int
+part2 Sim{p0 = Send x p0,                ..} = part2 Sim{q2 = q2|>x             , ..}
+part2 Sim{p1 = Send x p1,                ..} = part2 Sim{q1 = q1|>x, ctr = ctr+1, ..}
+part2 Sim{p0 = Recv _ f1, q1 = x :<| q1, ..} = part2 Sim{p0 = f1 x              , ..}
+part2 Sim{p1 = Recv _ f2, q2 = x :<| q2, ..} = part2 Sim{p1 = f2 x              , ..}
+part2 Sim{..}                                = ctr
+
+-- | Observable program execution effects
+data Command
+  = Send Integer Command              -- ^ Send integer
+  | Recv Integer (Integer -> Command) -- ^ Receive integer with old register value
+  | Done                              -- ^ Execution complete
+
+-- | Lens into a map of integers where a missing key is treated as a zero.
 reg :: String -> Lens' (Map String Integer) Integer
 reg r = at r . non 0
 
-program :: V.Vector [String] -> Map String Integer -> Command
-program cmds = step 0
+-- | Either lookup a register or return the value of a constant.
+(!) ::
+  Map String Integer {- ^ registers          -} ->
+  String             {- ^ number or register -} ->
+  Integer            {- ^ argument value     -}
+m ! k =
+  case readMaybe k of
+    Just v  -> v
+    Nothing -> view (reg k) m
+
+-- | Compute the effect of executing a program starting at the first instruction
+-- using the given map as the initial set of registers.
+runProgram ::
+  V.Vector [String]  {- ^ instructions      -} ->
+  Map String Integer {- ^ initial registers -} ->
+  Command            {- ^ program effect    -}
+runProgram cmds = step 0
   where
     step pc regs =
       case cmds V.!? pc of
-        Nothing -> Done
-        Just ["snd",x  ] -> Send    (regs!x) (step (pc+1) regs )
-        Just ["rcv",x  ] -> Receive (regs!x) (\i -> step (pc+1) (set (reg x) i regs) )
-        Just ["set",x,y] -> step (pc+1) (set  (reg x) (regs!y)         regs)
+        Nothing          -> Done
+        Just ["snd",x  ] -> Send (regs!x) (      step (pc+1)                regs)
+        Just ["rcv",x  ] -> Recv (regs!x) (\i -> step (pc+1) (set (reg x) i regs))
+        Just ["set",x,y] -> step (pc+1) (set  (reg x)        (regs!y)  regs)
         Just ["add",x,y] -> step (pc+1) (over (reg x) (+     (regs!y)) regs)
         Just ["mul",x,y] -> step (pc+1) (over (reg x) (*     (regs!y)) regs)
         Just ["mod",x,y] -> step (pc+1) (over (reg x) (`mod` (regs!y)) regs)
-        Just ["jgz",x,y] ->
-          let offset | regs!x > 0 = fromIntegral (regs!y)
-                     | otherwise  = 1
-          in step (pc + offset) regs
+        Just ["jgz",x,y] -> step (pc+o) regs
+          where
+            o | regs!x > 0 = fromIntegral (regs!y)
+              | otherwise  = 1
