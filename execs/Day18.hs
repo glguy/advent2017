06464f345ef8aa8cc3b538b6316a1e4759f11c87
Eric Mertens <emertens@gmail.com>
2017-12-17 22:13:58 -0800

    Cleanup checkpoint for day 18

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -1,3 +1,4 @@
+{-# Language RankNTypes #-} -- Lens' type synonym use
 {-|
 Module      : Main
 Description : Day 18 solution
@@ -8,25 +9,27 @@ Maintainer  : emertens@gmail.com
 module Main where
 
 import Advent
-import Control.Lens
+import Control.Lens (at, non, set, over, Lens')
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Vector as V
 import Text.Read (readMaybe)
+import Data.Sequence (Seq((:<|)), (|>))
 
 main :: IO ()
 main =
   do input <- V.fromList . map words . lines <$> getInput 18
      print $ firstSend (program input Map.empty)
-     print $ run 0 [] (program input (Map.singleton "p" 0))
-                   [] (program input (Map.singleton "p" 1))
+     print $ run 0 mempty (program input (Map.singleton "p" 0))
+                   mempty (program input (Map.singleton "p" 1))
 
-run :: Integer -> [Integer] -> Command -> [Integer] -> Command -> Integer
-run cnt (x:xs) p1 ys (Receive _ p2) = run cnt xs p1 ys (p2 x)
-run cnt xs     p1 ys (Send    y p2) = run (cnt+1) xs p1 (ys++[y]) p2
-run cnt xs (Receive _ p1) (y:ys) p2 = run cnt xs (p1 y) ys p2
-run cnt xs (Send    x p1) ys     p2 = run cnt (xs ++ [x]) p1 ys p2
-run cnt _  _  _ _                   = cnt
+-- | Count the number of sends by program #1
+run :: Integer -> Seq Integer -> Command -> Seq Integer -> Command -> Integer
+run cnt (x:<|xs) p1 ys (Receive _ p2) = run cnt     xs      p1     ys        (p2 x)
+run cnt xs       p1 ys (Send    y p2) = run (cnt+1) xs      p1     (ys |> y) p2
+run cnt xs (Receive _ p1) (y:<|ys) p2 = run cnt     xs      (p1 y) ys        p2
+run cnt xs (Send    x p1) ys       p2 = run cnt     (xs|>x) p1     ys        p2
+run cnt _ _ _ _                       = cnt
 
 (!) :: Map String Integer -> String -> Integer
 (!) m k =
@@ -45,20 +48,22 @@ firstSend = go 0
     go s (Receive 0 p) = go s (p 0)
     go s (Receive _ p) = s
 
+reg :: String -> Lens' (Map String Integer) Integer
+reg r = at r . non 0
+
 program :: V.Vector [String] -> Map String Integer -> Command
 program cmds = step 0
   where
     step pc regs =
       case cmds V.!? pc of
         Nothing -> Done
-        Just ["snd",r] -> Send (regs ! r) (step (pc+1) regs )
-        Just ["set",r,v] -> step (pc+1) (set (at r . non 0) (regs ! v) regs)
-        Just ["add",r,v] -> step (pc+1) (over (at r . non 0) (+(regs ! v)) regs)
-        Just ["mul",r,v] -> step (pc+1) (over (at r . non 0) (*(regs ! v)) regs)
-        Just ["mod",r,v] -> step (pc+1) (over (at r . non 0) (`mod`(regs ! v)) regs)
-        Just ["rcv",r] -> Receive (regs ! r) (\i -> step (pc+1) (set (at r . non 0) i regs) )
-        Just ["jgz",r,v] ->
-          let v' = fromIntegral (regs ! v) in
-          if (regs ! r) > 0
-            then step (pc + v') regs
-            else step (pc+1) regs
+        Just ["snd",x  ] -> Send    (regs!x) (step (pc+1) regs )
+        Just ["rcv",x  ] -> Receive (regs!x) (\i -> step (pc+1) (set (reg x) i regs) )
+        Just ["set",x,y] -> step (pc+1) (set  (reg x) (regs!y)         regs)
+        Just ["add",x,y] -> step (pc+1) (over (reg x) (+     (regs!y)) regs)
+        Just ["mul",x,y] -> step (pc+1) (over (reg x) (*     (regs!y)) regs)
+        Just ["mod",x,y] -> step (pc+1) (over (reg x) (`mod` (regs!y)) regs)
+        Just ["jgz",x,y] ->
+          let offset | regs!x > 0 = fromIntegral (regs!y)
+                     | otherwise  = 1
+          in step (pc + offset) regs
